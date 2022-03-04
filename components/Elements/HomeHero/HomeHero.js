e09832bb90ca0b4cbdd7f947a8447227ce13ace7
ggsales <giovanisales14@gmail.com>
2022-03-03 22:33:12 -0300

    init hero

---
+++ components/Elements/HomeHero/HomeHero.js
@@ -0,0 +1,20 @@
+
+
+
+export default function HomeHero() {
+    return (
+        <section className="hero">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-7">
+                        <h1>Texto</h1>
+                    </div>
+                    <div className="col-lg-5">
+                        <h1>Imagem</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import Homes from "../components/Elements/Home";
+
+import HomeHero from "../components/Elements/HomeHero/HomeHero";
 import Layout1 from "../components/Layout/layout1";
 
 
 export default function Home() {
   return (
     <Layout1>
-      <Homes/>
+      <HomeHero/>
     </Layout1>
   )
 }
