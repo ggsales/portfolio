695d1a9ec76e9d2b294c438477645bddb9855f52
ggsales <giovanisales14@gmail.com>
2022-03-07 19:30:00 -0300

    create introduction section

---
+++ components/Elements/Introduction/Introduction.js
@@ -0,0 +1,27 @@
+
+
+
+export default function Introduction() {
+    return (
+        <section className="introduction__area">
+            <h3 className="introduction__title">Introdução</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 offset-md-1 d-flex align-items-center justify-content-start">
+                        <h1>Sobre mim</h1>
+                        <hr />
+                    </div>
+                    <div className="col-md-9 offset-md-1   d-flex align-items-center justify-content-end">
+                        <h1>Formação</h1>
+                    </div>
+                    <div className="col-md-8 offset-md-1  d-flex align-items-center justify-content-start">
+                        <h1>Jornada como dev</h1>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 
 import HomeHero from "../components/Elements/HomeHero/HomeHero";
+import Introduction from "../components/Elements/Introduction/Introduction";
 import Layout1 from "../components/Layout/layout1";
 
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <Layout1>
       <HomeHero/>
+      <Introduction/>
     </Layout1>
   )
 }
