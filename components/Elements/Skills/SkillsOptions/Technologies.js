b8986de354158d93f65c510377a5d54bcba3697d
ggsales <giovanisales14@gmail.com>
2022-03-12 14:15:04 -0300

    create tech cards with animation

--- components/Elements/Skills/SkillsOptions/Technologies.js
+++ components/Elements/Skills/SkillsOptions/Technologies.js
@@ -1,9 +1,54 @@
 
 
-export default function Technologies(){
-    return(
+export default function Technologies() {
+    return (
         <>
-        
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 skills__technologies-box">
+                        <div className="row">
+                            <div className="col-sm-3 card-tech">
+                                <img src="./img/technologies/html.png" alt="html-icon" />
+                                <h6>HTML</h6>
+
+                            </div>
+                            <div className="col-sm-3 card-tech">
+                                <img src="./img/technologies/css-3.png" alt="css-icon" />
+                                <h6>CSS</h6>
+
+                            </div>
+                            <div className="col-sm-3 card-tech">
+                                <img src="./img/technologies/sass.png" alt="sass-icon" />
+                                <h6>SASS</h6>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-sm-3 card-tech">
+                                <img src="./img/technologies/js.png" alt="javascript-icon" />
+                                <h6>JAVASCRIPT</h6>
+                            </div>
+
+                            <div className="col-sm-3 card-tech">
+                                <img src="./img/technologies/reactjs.png" alt="react-icon" />
+                                <h6>REACTJS</h6>
+                            </div>
+
+                            <div className="col-sm-3 card-tech">
+                                <img src="./img/technologies/next-icon.png" alt="next-icon" />
+                                <h6>NEXTJS</h6>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-3 card-tech">
+                                <img src="./img/technologies/bootstrap.png" alt="bootstrap-icon" />
+                                <h6>BOOTSTRAP</h6>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
