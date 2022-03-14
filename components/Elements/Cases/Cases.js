bf68f70fa6b0861d0be5b2d197dca963388f137d
ggsales <giovanisales14@gmail.com>
2022-03-13 23:30:57 -0300

    create content case box and create link cases

--- components/Elements/Cases/Cases.js
+++ components/Elements/Cases/Cases.js
@@ -2,6 +2,7 @@ import Slider from "react-slick";
 import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Link from "next/dist/client/link";
 
 
 
@@ -48,19 +49,34 @@ export default function Cases() {
             <p className="cases__subtitle">Abaixo estão todos os meus cases desenvolvidos até hoje.</p>
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-8 offset-sm-2">
+                    <div className="col-sm-10 col-lg-8 offset-lg-2 offset-sm-1">
                         <Slider {...settings} className="carousel-cases">
-                            <div className="case">
+                            <div className="case case-mobile-1">
                                 <img src="./img/cases/fluabit-site.png" alt="fluabit-site" />
-                                <div className="cases__full-overlay"></div>
+                                <div className="cases__full-overlay">
+                                    <div className="cases__content-box-cases">
+                                        <h3>Website - Fluabit</h3>
+                                        <Link href="https://old.fluabit.com/"><button>Acessar</button></Link>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="case">
+                            <div className="case case-mobile-2">
                                 <img src="./img/cases/buffet-lavida.png" alt="Buffet-laVida-site" />
-                                <div className="cases__full-overlay"></div>
+                                <div className="cases__full-overlay">
+                                <div className="cases__content-box-cases">
+                                        <h3>Website - Buffet La Vida</h3>
+                                        <Link href="https://buffetlavida.com.br/"><button>Acessar</button></Link>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="case">
+                            <div className="case case-mobile-3">
                                 <img src="./img/cases/alurakut.png" alt="projeto-alurakut" />
-                                <div className="cases__full-overlay"></div>
+                                <div className="cases__full-overlay">
+                                <div className="cases__content-box-cases">
+                                        <h3>Alurakut - Imersão React Alura 2021</h3>
+                                        <Link href="https://alurakut-ggsales.vercel.app/login"><button>Acessar</button></Link>
+                                    </div>
+                                </div>
                             </div>
                         </Slider>
                     </div>
