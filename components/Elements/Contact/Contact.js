d10deffaefce56fd0ff9cfb8b7c053b3818a50a0
ggsales <giovanisales14@gmail.com>
2022-03-15 01:25:36 -0300

    apply title in contact

--- components/Elements/Contact/Contact.js
+++ components/Elements/Contact/Contact.js
@@ -10,12 +10,18 @@ export default function Contact() {
         <section className="contact">
             <div className="container">
                 <div className="row">
+                    <div className="col-12">
+                        <h3 className="contact__title">Contato</h3>
+                    </div>
+
+                </div>
+                <div className="row">
                     <div className="col-lg-6 offset-lg-1">
                         <div className="contact__form-area">
                             <Form />
                         </div>
                     </div>
-                    <div className="col-lg-5">
+                    <div className="col-lg-5 contact__links-btn-wrapper">
                         <div className="contact__btn-box">
                             <Link href="https://github.com/ggsales"><button className="fab fa-github btn-contact" /></Link>
                             <Link href="https://www.linkedin.com/in/giovanisales/"><button className="fab fa-linkedin btn-contact" /></Link>
