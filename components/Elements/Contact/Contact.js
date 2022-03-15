3b7e099bcd81b914e3276aa52625cc1a7b2c38aa
ggsales <giovanisales14@gmail.com>
2022-03-15 02:30:08 -0300

    fix bugs in mobile screen

--- components/Elements/Contact/Contact.js
+++ components/Elements/Contact/Contact.js
@@ -16,13 +16,16 @@ export default function Contact() {
 
                 </div>
                 <div className="row">
-                    <div className="col-lg-6 offset-lg-1">
+                    <div className="col-lg-6 offset-lg-1 col-xl-5 offset-xl-2">
                         <div className="contact__form-area">
+                            <h6 className="contact__form-title">Envie uma mensagem diretamente do site preechendo<br/> o formulário abaixo.</h6>
                             <Form />
                         </div>
                     </div>
-                    <div className="col-lg-5 contact__links-btn-wrapper">
+                    <div className="col-lg-4 col-xl-4 contact__links-btn-wrapper">
                         <div className="contact__btn-box">
+                            <h2 className="contact__btn-box__title">Ou se preferir...</h2>
+                            <h3 className="contact__btn-box__subtitle">Entre em contato pelos links abaixo.</h3>
                             <Link href="https://github.com/ggsales"><button className="fab fa-github btn-contact" /></Link>
                             <Link href="https://www.linkedin.com/in/giovanisales/"><button className="fab fa-linkedin btn-contact" /></Link>
                         </div>
