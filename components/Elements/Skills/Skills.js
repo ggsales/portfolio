b8810b2faa0634091b11151e0b1bb1b3a000dabd
ggsales <giovanisales14@gmail.com>
2022-03-17 20:37:18 -0300

    apply new division column

--- components/Elements/Skills/Skills.js
+++ components/Elements/Skills/Skills.js
@@ -24,22 +24,22 @@ export default function Skills(){
         <section className="skills">
             <div className="container">
                 <div className="row">
-                    <div className="col-12">
+                    <div className="col-md-8 offset-md-2">
                         <h1 className="skills__title">Conhecimentos</h1>
-                        <p className="skills__subtitle">Abaixo estão minhas habilidades como programador Front-end, e com quais tecnologias trabalho.</p>
+                        <p className="skills__subtitle">Abaixo estão minhas habilidades como programador Front-end, e meu desempenho nos modelos de desenvolvimento web. E também com quais tecnologias trabalho.</p>
                     </div>
                 </div>
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-10 offset-lg-1 mt-80">
                             <div className="row">
-                                <div className="col-lg-5">
+                                <div className="col-lg-12">
                                     <div className="skills__btn-box">
                                         <button onClick={() => setTabCurrent("habilidade")}  className={tabCurrent === "habilidade" ? "actived" : undefined}>Habilidades</button>
                                         <button onClick={() => setTabCurrent("tecnologias")} className={tabCurrent === "tecnologias" ? "actived" : undefined}>Tecnologias</button>
                                     </div>
                                 </div>
-                                <div className="col-lg-7">
+                                <div className="col-lg-8 offset-lg-2">
                                     {tabCurrent === "habilidade" && <Ability/> }
                                     {tabCurrent === "tecnologias" &&  <Technologies/> }
                                 </div>
